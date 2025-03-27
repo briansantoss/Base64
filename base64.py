ef79ae8feac0de1f78b7b960a2cdbd398db0de60
142011551+briansantoss@users.noreply.github.com <142011551+briansantoss@users.noreply.github.com>
2025-03-26 21:07:01 -0300

    Implementando tratamento com padding

--- base64.py
+++ base64.py
@@ -1,10 +1,10 @@
-def base64_enc(word):
+def base64_enc(word: str):
     # Define o tamanho dos blocos (3 bytes)
     block_bytes_len = 3
     # Define o tamanho dos miniblocos/grupos (6 bits)
     groups_bits_len = 6
 
-    padding_char = '='
+    padding_char = '='# Define o caractere usado para preenchimento
 
     table_ranges = {
         'ucase_letters': ('A', 0, 25), # Armazena o intervalo reservado para letras maiúsculas (A-Z)
@@ -15,11 +15,15 @@ def base64_enc(word):
     # Obtém a representação em byte de cada caractere
     word_bytes = [f'{ord(char):08b}' for char in word]
 
+    # Calcula a quantidade de preenchimento (caso necessário)
+    padding_num = block_bytes_len - len(word_bytes) % block_bytes_len if len(word_bytes) % block_bytes_len else 0
+
     # Separa os bytes da palavra em blocos de 3 bytes
     blocks = [''.join(word_bytes[i: i + block_bytes_len]) for i in range(0, len(word_bytes), block_bytes_len)]
 
     # Organiza cada um dos blocos de 3 bytes em 4 grupos de 6 bits cada
-    blocks = [[''.join(block[i: i + groups_bits_len]) for i in range(0, len(block), groups_bits_len)] for block in blocks]
+    # realizando o preenchimento a direita com zeros caso preciso
+    blocks = [[''.join(block[i: i + groups_bits_len].ljust(6, '0')) for i in range(0, len(block), groups_bits_len)] for block in blocks]
 
     result = ''
     for block in blocks:
@@ -31,4 +35,4 @@ def base64_enc(word):
                     normalized_num = num - start # Calculando a distância/deslocamento do número para o primeiro do intervalo
                     result += chr(normalized_num+ ord(first_char)) # Concatenando o caractere obtido por chr ao resultado da codificação
                     break
-    return result
+    return result + padding_char * padding_num
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import base64
 
 word = input('Enter a word: ')
-print(base64.base64_enc(word))
+print('Result:', base64.base64_enc(word))
